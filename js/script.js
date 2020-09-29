ec265ba53cdeb79d604472ba49f9e8100b26fedf
Natalia Ramirez <ntlc.ramirez@gmail.com>
2020-09-29 18:34:50 +1300

    first fix

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('js');
+
+
+$('#drop-nav').hide();
+
+$(document).ready(function(){
+
+$('.fa-bars').click(function(){
+  $('#drop-nav').toggle();
+});
+
+
+
+}); //document.ready() ends
